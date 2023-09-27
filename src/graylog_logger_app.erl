b2426644da15fd114270f0c3b65138d4f7c0bd32
Byron Wang <byronpc1@gmail.com>
2023-09-27 13:37:38 +0800

    Remove logger handler before stopping app to avoid failing_handler errors

--- src/graylog_logger_app.erl
+++ src/graylog_logger_app.erl
@@ -13,4 +13,6 @@ start(_StartType, _StartArgs) ->
     graylog_logger_sup:start_link().
 
 stop(_State) ->
+    [{handler, HandlerId, _, _}] = application:get_env(graylog_logger, logger, []),
+    logger:remove_handler(HandlerId),
     ok.
